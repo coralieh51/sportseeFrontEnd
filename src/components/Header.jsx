03a3388c9fb52d9aba1ecaeaec99b0351d1cb48d
Coralieh <hubert.coralie@gmail.com>
2022-02-13 19:33:39 +0100

    cleaning files

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <header>
+            <h1>Bonjour <span>Name</span></h1>
+            <aside>Félicitations ! Vous avez explosé vos obectifs hier !</aside>
+        </header>
+    )
+}
+
+export default Header;
